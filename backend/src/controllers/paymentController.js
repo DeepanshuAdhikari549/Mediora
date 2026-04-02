2126345ad4b8ff89f173db1afa083786867b6cc2
DeepanshuAdhikari549 <deepanshuadhikari549@gmail.com>
2026-04-02 15:28:16 +0530

    Safeguard global stripe instantiation to prevent Render crash if key is missing locally

--- backend/src/controllers/paymentController.js
+++ backend/src/controllers/paymentController.js
@@ -1,7 +1,7 @@
 import Stripe from 'stripe';
 import Booking from '../models/Booking.js';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+const stripe = process.env.STRIPE_SECRET_KEY ? new Stripe(process.env.STRIPE_SECRET_KEY) : null;
 
 export const createPaymentIntent = async (req, res) => {
     try {
